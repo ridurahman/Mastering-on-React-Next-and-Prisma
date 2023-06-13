12ff805c612db611b684959a00422cfa648547ad
ridurahman <lindaridu@gmail.com>
2023-06-13 21:18:45 +0600

    First Commit

---
+++ Module 02/JS_Practice/function.js
@@ -0,0 +1,8 @@
+function add_Declaration(a, b) {
+    return a + b;
+}
+
+const add_Expression = function (a, b) {
+    return a + b;
+};
+
---
+++ Module 02/JS_Practice/index.js
@@ -0,0 +1,10 @@
+var arr= ["Saab", "Volvo", "BMW"];
+
+arr.forEach(item=>{
+    console.log(`${item}`)
+});
+console.log("hello");
+
+for(const item of arr){
+    console.log(item);
+}
